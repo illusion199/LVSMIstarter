aba4d1d1637ec93ceae35ab2ea9f800040ba658b
@deveansari <you@example.com>
2019-05-15 23:05:50 +0600

    little design change on upload profile image

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -6,11 +6,13 @@
     <div class="alert alert-danger">{{ session()->get('error') }}</div>
     @endif
     @foreach ($avatar as $item)
-       <div class="text-center">{{$item}} </div>
+       <div>
+           <img src="{{$item->getUrl()}}" alt="" class="imgcls">
+        </div>
     @endforeach
     <form method="POST" action="{{route('avatar.store')}}" enctype="multipart/form-data">
         @csrf        
-        <div class="input-group mb-3">
+        {{-- <div class="input-group mb-3">
             <div class="input-group-prepend">
                 <input type="submit" class="btn btn-info" value="upload">
             </div>
@@ -18,8 +20,18 @@
                 <input type="file" name="avatar" class="custom-file-input">
                 <label class="custom-file-label">Choose file</label>              
             </div>            
+        </div> --}}
+
+        <div class="form-group">
+            <div class="col-sm-offset-2 col-sm-10">
+                <label class="file-upload">
+                    Browse for file ... <input type="file" name="avatar"/>
+                </label>
+                <button type="submit" class="btn btn-info">Upload</button>
+            </div>
         </div>
     </form> 
+   
     
 </div>
 @endsection
